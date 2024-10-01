6620eafd29d80e46f7f4d3cc67e2a7e57ec7491b
s1rcody66 <cody200600@gmail.com>
2024-10-01 19:16:47 +0300

    completelab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(driver => driver.toLowerCase().startsWith (letters.toLowerCase()));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
+    
+
+    
+
